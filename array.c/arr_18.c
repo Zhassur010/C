970d58c6295eda2bcd4c913e47a09de9a85541c3
Zhassur Tashaliyev <zhassurtashaliyev@gmail.com>
2022-07-23 09:19:03 +0500

    array.c commit

--- array.c/arr_18.c
+++ array.c/arr_18.c
@@ -3,10 +3,28 @@
 
 int main(){
 
-    int n=6, k;
+    int k;
     int a[11];
 
+    for(int k=0; k<10; k++){
+        printf("a[%d]=", k+1);
+        scanf("%d", &a[k]);
+    }
+    for(int k=0; a[k]>=a[9]; k++){
+        if(k==10){
+            printf("0\n");
+        }else{
+            printf("%d\n", a[k]);
+        }
+    }
 
+    printf("%d : %d\n", k, a[k]);
+
+//    if(k==10){
+//        printf("0\n");
+//    }else{
+//        printf("%d\n", a[k]);
+//    }
 
     return 0;
 }
